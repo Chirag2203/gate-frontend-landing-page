423c32816599d270d9b71e01470deae074069daa
Chirag Rajput <103127634+Chirag2203@users.noreply.github.com>
2024-10-29 18:18:32 +0530

    auth side bar added

--- src/components/shared/wrappers/AuthWrapper.tsx
+++ src/components/shared/wrappers/AuthWrapper.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <div className="w-1/2">
         <AuthInfoSideBar />
       </div>
---
+++ src/constants/data.ts
@@ -0,0 +1,24 @@
+export const testimonialsForAuthSideBar = 
+[
+    {
+        id: 1,
+        name: "John Doe",
+        title: "AIR 1 GATE CSE 2024",
+        image: "https://randomuser.me/api/portraits",
+        quote: "I am so grateful for the support from my mentors and the entire team at GATE CSE 2024. I couldn't have done it without them!",
+    },
+    {
+        id: 2,
+        name: "Ram Doe",
+        title: "AIR 1 GATE CSE 2024",
+        image: "https://randomuser.me/api/portraits",
+        quote: "I am so grateful for the support from my mentors and the entire team at GATE CSE 2024. I couldn't have done it without them!",
+    },
+    {
+        id: 3,
+        name: "Sham Doe",
+        title: "AIR 1 GATE CSE 2024",
+        image: "https://randomuser.me/api/portraits",
+        quote: "I am so grateful for the support from my mentors and the entire team at GATE CSE 2024. I couldn't have done it without them!",
+    },
+];
