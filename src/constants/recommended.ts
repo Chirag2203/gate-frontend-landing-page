dc8483aee39903070dbd5669d9ec6b02c2fa28f0
Chirag Rajput <103127634+Chirag2203@users.noreply.github.com>
2024-11-14 02:48:13 +0530

    removed image from recommended

--- src/constants/recommended.ts
+++ src/constants/recommended.ts
@@ -1,6 +1,6 @@
 import one from "@/assets/recommended/1.png"
 import two from "@/assets/recommended/2.png"
-import three from "@/assets/recommended/3.png"
+// import three from "@/assets/recommended/3.png"
 import four from "@/assets/recommended/4.png"
 import five from "@/assets/recommended/5.png"
 import six from "@/assets/recommended/6.png"
@@ -12,5 +12,5 @@ import eleven from "@/assets/recommended/11.png"
 import twelve from "@/assets/recommended/12.png"
 import thirteen from "@/assets/recommended/13.png"
 
-export const setOne = [one, two, three, four, five, six, seven, eight]
+export const setOne = [one, two, four, five, six, seven, eight]
 export const setTwo = [nine, ten, eleven, twelve, thirteen]
